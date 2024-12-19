db6572158c92c19f7551a83dc68da91aa252a4a3
Sergey Antonov <js@vl-soft.com>
2024-12-19 23:10:18 +0300

    (fix) setup ESLint & Prettier plugins and formating all code again #2

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    'plugin:prettier/recommended',
-    'prettier/vue',
     'prettier'
   ],
   plugins: ['prettier'],
